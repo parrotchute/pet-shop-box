8198f3580245633be5ae5ac3ff0b7214f3053f15
Mei-Jing Tan <mei-jing.tan@versent.com.sg>
2022-01-12 23:30:23 +0800

    add contract and more logic

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,9 @@
+
+const PrivateKeyProvider = require("@truffle/hdwallet-provider");
+
+// insert the private key of the account used in metamask eg: Account 1 (Miner Coinbase Account)
+const privateKey = "REPLACE_PRIVATE_KEY";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,6 +15,12 @@ module.exports = {
     },
     develop: {
       port: 8545
+    },
+    quickstartWallet: {
+      provider: () => new PrivateKeyProvider(privateKey, "http://localhost:8545"),
+      network_id: "*",
+      type: "quorum",
+      gasPrice: 0
     }
   }
 };
